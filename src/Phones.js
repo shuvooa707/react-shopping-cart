7c9f2d7fcff968719bd32ed7847ab4e658a7a5c5
shuvooa707 <shuvooa707@gmail.com>
2022-01-23 22:46:21 +0600

    1 file modified

--- src/Phones.js
+++ src/Phones.js
@@ -3,6 +3,6 @@ let phones = [
   { id: '2', name: 'Galaxy S8', price: 60000 },
   { id: '3', name: 'Galaxy S9', price: 80000 },
   { id: '4', name: 'OnePlus 8T', price: 45000 },
-  { id: '4', name: 'Sony Xperia', price: 30000 },
+  { id: '5', name: 'Sony Xperia', price: 30000 },
 ];
 export default { phones };
